800ce421f76112d822a70c0202096cf2baf3a8cc
chris <64315364+56157129@users.noreply.github.com>
2020-04-25 20:44:54 +0700

    Update and rename env.example.js to env.js

---
+++ env.js
@@ -0,0 +1,11 @@
+window.settings = {
+  // 站点标题
+  title: 'Go2Free',
+  host: 'http://suo.im/6sr3Mc',
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  background_url: 'https://www.bing.com/th?id=OHR.FalklandRockhoppers_ROW9057491810_1920x1080.jpg&rf=LaDigue_1920x1080.jpg'
+}
